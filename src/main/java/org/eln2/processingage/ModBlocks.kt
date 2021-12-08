0bcebc5827ea7dadd2747ad7a1d3f5d1e95e0506
Jared Dunbar <jrddunbr@ja4.org>
2021-12-07 23:22:00 -0500

    Preparing for 1.18 update

--- src/main/java/org/eln2/processingage/ModBlocks.kt
+++ src/main/java/org/eln2/processingage/ModBlocks.kt
@@ -3,7 +3,12 @@ package org.eln2.processingage
 import net.minecraft.block.AbstractBlock
 import net.minecraft.block.Block
 import net.minecraft.block.material.Material
+import net.minecraft.client.renderer.color.IItemColor
 import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.util.text.ITextComponent
+import net.minecraftforge.client.event.ColorHandlerEvent
+import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.registries.ForgeRegistries
 import thedarkcolour.kotlinforforge.forge.KDeferredRegister
 
@@ -15,7 +20,29 @@ object ModBlocks {
         Block(AbstractBlock.Properties.of(Material.STONE))
     }
 
-    val ORE_CHUNK by ITEMS.registerObject("ore_chunk") {
-        Item(Item.Properties())
+
+    val oreChunkList = mutableListOf("fake_ore")
+
+    init {
+        oreChunkList.forEach {
+            ITEMS.registerObject(it) {
+                OreChunk(it, 0xFF, 0x11)
+            }
+        }
+    }
+}
+
+class OreChunk(private val name: String, private val rockColor: Int, private val oreColor: Int): Item(Properties()), IItemColor {
+    override fun getColor(p0: ItemStack, p1: Int): Int {
+        return if (p1 == 0) rockColor else oreColor
+    }
+
+    override fun getDescriptionId(): String {
+        return "item.processingage.$name"
+    }
+
+    @SubscribeEvent
+    fun registerItemColors(event: ColorHandlerEvent.Item) {
+        event.itemColors.register(this, this)
     }
 }
